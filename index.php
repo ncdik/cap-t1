fa7531e484c3a1e6cc80d68f6e1b93c5dad80a58
Mikhail Zhukov <mikhail.zhukov@sibers.com>
2018-11-14 11:01:54 +0700

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+function hello()
+{
+    echo 'hello';
+}
+function world()
+{
+    echo 'world';
+}
+function space()
+{
+    echo ' ';
+}
+function endl()
+{
+    echo chr(10);
+}
+
+hello();
+space();
+world();
+endl();
